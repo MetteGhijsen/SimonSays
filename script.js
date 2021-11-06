3241bb38f9d11aaf85aedb8d93ecb75659a4ab46
I481695 <mette.ghijsen@student.fontys.nl>
2021-11-06 18:48:55 +0100

    knipperprobleem opgelost

--- script.js
+++ script.js
@@ -10,110 +10,171 @@ let buttonGreen = document.getElementById('buttonGreen');
 let buttonBlue = document.getElementById('buttonBlue');
 let scorePlayer = document.getElementById('winsplayer');
 
+function startRound(){
+    console.log("start round");
+    generatedSequence = [];
+}
+
 function PlayGame() {
     if (currentLevel === 1) {
         GenerateSequence();
         ShowSequence();
+        console.log("playgame aangeroepen");
     }
 }
 
 function GenerateSequence() {
+    console.log("generatesequence aangeroepen");
     for (let i = 0; i < maxLevel; i++) {
         generatedSequence[i] = Math.floor(Math.random() * 4);
     }
 }
 
 function ShowSequence() {
-    for (let i = 0; i < 10; i++) {
-        switch (generatedSequence[i]) {
-            case 0: //rood
-                buttonRed.style.opacity = "100%";
-                setTimeout(function () {
-                    buttonRed.style.opacity = "";
-                }, difficulty);
-                //playNote(310, 500);
-                break;
-            case 1: //geel
-                buttonYellow.style.opacity = "100%";
-                setTimeout(function () {
-                    buttonYellow.style.opacity = "";
-                }, difficulty);
-                //playNote(252, 500);
-                break;
-            case 2: //groen
-                buttonGreen.style.opacity = "75%";
-                setTimeout(function () {
-                    buttonGreen.style.opacity = "";
-                }, difficulty);
-                //playNote(415, 500);
-                break;
-            case 3: //blauw
-                buttonBlue.style.opacity = "100%";
-                setTimeout(function () {
-                    buttonBlue.style.opacity = "";
-                }, difficulty);
-                //playNote(209, 500);
-                break;
-        }
+    console.log("showsequence aangeroepen");
+    for (let i = 0; i < currentLevel; i++) {
+        buttonRed.style.opacity = "";
+        buttonYellow.style.opacity = "";
+        buttonGreen.style.opacity = "";
+        buttonBlue.style.opacity = "";
+        setTimeout(function (){
+            switch (generatedSequence[i]) {
+                case 0: //rood
+                    flashTile(buttonRed);
+                    playSound(buttonRed);
+                    break;
+                case 1: //geel
+                    flashTile(buttonYellow);
+                    playSound(buttonYellow);
+                    break;
+                case 2: //groen
+                    flashTile(buttonGreen);
+                    playSound(buttonGreen);
+                    break;
+                case 3: //blauw
+                    flashTile(buttonBlue);
+                    playSound(buttonBlue);
+                    break;
+            }
+        },difficulty*i);
+    }
+}
+
+function flashTile(tile){
+    tile.style.opacity = "100%";
+    setTimeout(function (){
+        tile.style.opacity = "";
+    },difficulty)
+}
+
+function playSound(hok){
+    switch(hok){
+        case buttonRed:
+            playNote(310, 500);
+            break;
+        case buttonYellow:
+            playNote(252, 500);
+            break;
+        case buttonGreen:
+            playNote(415, 500);
+            break;
+        case buttonBlue:
+            playNote(209, 500);;
+            break;
     }
 }
 
 function Choice(color) {
+    console.log("choice aangeroepen");
     for (let i = 0; i < currentLevel; i++) {
-        playerSequence[i] = color;
         switch (color) {
             case 0:
-                playNote(310, 500);
+                playSound(buttonRed);
+                playerSequence[i] = 0;
+                compareSequence(i);
                 break;
             case 1:
-                playNote(252, 500);
+                playSound(buttonYellow);
+                playerSequence[i] = 1;
+                compareSequence(i);
                 break;
             case 2:
-                playNote(415, 500);
+                playSound(buttonGreen);
+                playerSequence[i] = 2;
+                compareSequence(i);
                 break;
             case 3:
-                playNote(209, 500);
+                playSound(buttonBlue);
+                playerSequence[i] = 3;
+                compareSequence(i);
                 break;
-        }
-
-        if (generatedSequence[i] !== playerSequence[i]) {
-            WrongAnswer();
-        } else {
-            GoodAnswer();
         }
     }
 }
 
+function compareSequence(i){
+    setTimeout(function (){
+        if (generatedSequence[i] === playerSequence[i]) {
+            GoodAnswer();
+        }
+        else{
+            WrongAnswer();
+        }
+    },1000);
+}
+
+function clearPlayerSequence(){
+    console.log("clearplayersequence aangeroepen");
+    playerSequence = [];
+}
+
 function GoodAnswer() {
+    console.log("goodanswer");
     BlinkingLights(1);
-    playNote(420, 500);
+    playNote(100, 500);
+    playNote(200, 500);
+    playNote(300, 500);
 
     if (currentLevel < maxLevel) {
         currentLevel++;
         scorePlayer.innerHTML = currentLevel;
         difficulty -= 50;
-        ShowSequence();
+
+        setTimeout(function (){
+            ShowSequence();
+        },1500);
     }
 }
 
 function WrongAnswer() {
+    console.log("wronganswer aangeroepen");
     BlinkingLights(3);
     playNote(100, 500);
+    ResetScore();
+}
+
+function ResetScore(){
+    console.log("resetscore aangeroepen");
+    currentLevel = 1;
+    difficulty = 1000;
+    clearPlayerSequence();
 }
 
 function BlinkingLights(frequency) {
-    for (let i; i < frequency; i++) {
-        buttonRed.style.opacity = "100%";
-        buttonYellow.style.opacity = "100%";
-        buttonGreen.style.opacity = "75%";
-        buttonBlue.style.opacity = "100%";
+    for (let i=0; i < frequency; i++) {
+        setTimeout(function (){
+            buttonRed.style.opacity = "100%";
+            buttonYellow.style.opacity = "100%";
+            buttonGreen.style.opacity = "75%";
+            buttonBlue.style.opacity = "100%";
 
-        setTimeout(() => {
-            buttonRed.style.opacity = "";
-            buttonYellow.style.opacity = "";
-            buttonGreen.style.opacity = "";
-            buttonBlue.style.opacity = "";
-        }, 500);
+            setTimeout(() => {
+                buttonRed.style.opacity = "";
+                buttonYellow.style.opacity = "";
+                buttonGreen.style.opacity = "";
+                buttonBlue.style.opacity = "";
+            }, 900);
+        },1000*i);
     }
 }
 
